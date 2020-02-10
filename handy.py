6117bc5127c9e62c541e900e196c50947741fb2f
Kyle Halladay <k.mj.halladay@gmail.com>
2020-02-10 08:24:03 -0600

    cleaned up the existing logic a bit

--- handy.py
+++ handy.py
@@ -1,10 +1,20 @@
 from blessed import Terminal
 from signal import signal, SIGWINCH
 from datetime import datetime
+from enum import Enum
 import re 
 
 term = Terminal()
 command_history = []
+
+class Pattern(Enum):
+    Arithemtic = 1,
+    ArithmeticWithComment = 2
+
+class Operator(Enum):
+    NotAnOperator = 0,
+    Addition = 1,
+    Subtraction = 2
 
 def resize_handler(signum, frame):
     """handles terminal resize"""
@@ -38,39 +48,70 @@ def b2h(bin_str):
 def b2d(bin_str):
     return (int(bin_str,2))
 
-def parseNumericValue(valueStr, radix, regex_pattern):
+def parse_operator(input_str):
+    if input_str == '+':
+        return Operator.Addition
+    
+    if input_str == "-":
+        return Operator.Subtraction
+    
+    return Operator.NotAnOperator
+
+def full_match(reg_pattern, input_str):
+    match = re.match(reg_pattern, input_str)
+    return match and match.end() == len(input_str)
+
+def is_hex(input_str):
+    return full_match("[a-f0-9]+",input_str.lower())
+
+def is_binary(input_str):
+    return full_match("[0-1]+", input_str.lower())
+
+def is_decimal(input_str):
+    return full_match("[0-9]+", input_str.lower())
+
+def is_numeric(input_str):
+    lc_input = input_str.lower()
+    if lc_input.startswith('0x') or lc_input.startswith('0b'):
+        return True
+    return is_decimal(input_str)
+
+def parseNumericValue(valueStr):
     lc_val = valueStr.lower()
-    reg_match = re.match(regex_pattern, lc_val)
+    radix = 10;
+
+    if (lc_val.startswith('0x')):
+        if is_hex(lc_val[2:]):
+            radix = 16
+        else:
+             return "Invalid Hex Number"
+    
+    if (lc_val.startswith('0b')):
+        if is_binary(lc_val[2:]):
+            radix = 2
+        else:
+            return "Invalid Binary Number"
+
+    if radix == 10 and is_decimal(lc_val) == False:
+        return "Invalid Decimal Number"
 
     parse_funcs = {16: [h2d, h2b], 10: [d2h, d2b], 2: [b2d, b2h]}
 
-    if reg_match and reg_match.end() == len(lc_val):
-        return str(parse_funcs[radix][0](lc_val)) + ", " + str(parse_funcs[radix][1](lc_val))
-    else:
-        return "Invalid Hex Input" if radix == 16 else "Invalid Binary Input" if radix == 2 else "Invalid Decimal Input"
+    return str(parse_funcs[radix][0](lc_val)) + ", " + str(parse_funcs[radix][1](lc_val))
 
 def eval(inputStr):
     split_input = inputStr.split()
     outputStr = datetime.now().strftime("%d/%m/%y %H:%M:%S") +": "
 
-    if len(split_input) == 1:
-        lc_input = split_input[0].lower()
-
-        if lc_input.startswith('0x'):
+    # there are only really two options, either an input string is a numeric operation
+    # (meaning it starts with a numeric), or it's a string that we just write to the log
+    if is_numeric(split_input[0]):
+        if len(split_input) == 1: #if there's only 1 numeric value, just convert it
             outputStr += split_input[0] + " -> "
-            outputStr += parseNumericValue(lc_input[2:], 16, "[a-f0-9]+")
-
-        elif lc_input.startswith('0b'):
-            outputStr += split_input[0] + " -> "
-            outputStr += parseNumericValue(lc_input[2:], 2, "[0-1]+")
-        else:
-            reMatch = re.match("[0-9]+", lc_input)
-            if reMatch and reMatch.end() == len(lc_input):
-                outputStr += split_input[0] + " -> "
-                outputStr += parseNumericValue(lc_input, 10, "[0-9]+")
-            else:
-                outputStr += inputStr
-    else:
+            outputStr += parseNumericValue(split_input[0])
+        else: # otherwise we might have an arithmetic expression, or an inline comment, or both
+            outputStr += inputStr
+    else: #if the first input isn't numeric, it's just a string
         outputStr += inputStr
         
     command_history.append(outputStr)
